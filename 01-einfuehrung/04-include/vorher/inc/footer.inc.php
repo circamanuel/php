16290e5ddc79ef79887bf3131b41731013d5ff72
circamanuel <163194187+MIDDASTREE@users.noreply.github.com>
2025-01-07 22:42:29 +0100

    Section 2

---
+++ 01-einfuehrung/04-include/vorher/inc/footer.inc.php
@@ -0,0 +1,9 @@
+</main>
+
+<footer>
+    <img style="border-radius: 5px; max-width: 100%; width: 700px;" src="images/pexel.jpg"
+    <p>Ein erstes Projekt vom PHP Kurs</p>
+</footer>
+
+</body>
+</html>
---
+++ 01-einfuehrung/04-include/vorher/inc/header.inc.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="styles/simple.css">
+    <title><?php echo $pageTitle;?></title>
+</head>
+<body>
+<header>
+    <h1>Das moderne Reisebüro</h1>
+
+    <nav>
+        <a href="./">Startseite</a>
+        <a href="helsinki.php">helsinki</a>
+        <a href="Brasil.php">Brasil</a>
+    </nav>
+</header>
+
+<main>
+    <h2><?php echo $pageTitle; ?></h2>
