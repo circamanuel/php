ba875977768eae05e5c6a699ddf466f842e31477
manuel <manu4126@gmail.com>
2025-01-12 19:37:46 +0100

    finished section five

--- 05-projekt-bildergalerie/01-aufgabe/image.php
+++ 05-projekt-bildergalerie/01-aufgabe/image.php
@@ -11,6 +11,28 @@
 <body>
     <main>
 
+        <?php
+         include 'inc/data.php';
+         include 'inc/functions.php';
+
+        if (isset($_GET['image'])) {
+            $key = $_GET['image'];
+
+            if (array_key_exists($key, $imageTitles)) {
+                $title = $imageTitles[$key];
+                $text = $imageDescs[$key];
+
+                echo "<h2>$title</h2><br>";
+                echo "<p>$text</p>";
+                echo "<img src='./images/" . e($key). "' alt='" . e($title) ."'>";
+
+            }
+            echo "<a href='index.php'>Back</a>";
+        }
+
+
+        ?>
+
     </main>
 </body>
 </html>
--- 05-projekt-bildergalerie/01-aufgabe/index.php
+++ 05-projekt-bildergalerie/01-aufgabe/index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" type="text/css" href="./styles/simple.css">
+    <link rel="stylesheet" type="text/css" href="./styles/gallery.css">
+
     <title>PHP-Kurs</title>
 </head>
 <body>
@@ -13,7 +15,17 @@
         <?php
             include 'inc/data.php';
             include 'inc/functions.php';
-        ?>
+
+       ?>
+
+    <div class="gallery-container">
+            <?php foreach($imageTitles AS $imageSrc =>$imageTitle): ?>
+             <a class="gallery-item" href="image.php?<?php echo http_build_query(['image' => $imageSrc]); ?>">
+                 <h4><?php echo e($imageTitle); ?></h4>
+                 <img src="images/<?php echo urlencode($imageSrc); ?>" alt="<?php echo e($imageTitle); ?>" />
+             </a>
+         <?php endforeach; ?>
+        </div>
     </main>
 </body>
 </html>
