004ec36776c5f6938dea1f0dfb29460183680538
manuel <manu4126@gmail.com>
2025-01-19 18:18:55 +0100

    finished part 7

--- 07-projekt-namen/01-ausgangsbasis/name.php
+++ 07-projekt-namen/01-ausgangsbasis/name.php
@@ -11,14 +11,69 @@ foreach($names AS $nameArray) {
         $filteredPersonList[] = $nameArray;
     }
 }
-var_dump($filteredPersonList);
+
 ?>
 
-<?php //foreach ($filtered[] AS $name => $_): ?>
-<!--    --><?php //echo $name; ?>
-<?php //endforeach; ?>
+<?php if (!empty($filteredPersonList)): ?>
+<h2>Geburtsstatistiken fuer <?php echo $currentName; ?></h2>
 
-<?php
-
-include 'views/footer.php';
+<?php 
+$chartYears = [];
+$chartCounts = [];
+foreach($filteredPersonList AS $nameArray) {
+  $chartYears[] = $nameArray['year'];
+  $chartCounts[] = $nameArray['count'];
+}
 ?>
+
+  <script type="text/javascript" src="scripts/chart.js"></script>
+
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script>
+        const ctx = document.getElementById('myChart');
+
+        new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: <?php echo json_encode($chartYears); ?>,
+                datasets: [{
+                    label: '# babies',
+                    data: <?php echo json_encode($chartCounts); ?>,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
+
+
+  <table>
+    <thead>
+      <tr>
+        <th>Jahr</th>
+        <th>Anzahl Geburten</th>
+      <tr>
+    <thead>
+    <tbody>
+      <?php foreach($filteredPersonList AS $nameArray): ?>
+        <tr>
+          <td><?php echo $nameArray['year']; ?></td>
+          <td><?php echo $nameArray['count']; ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+<?php endif; ?>
+
+<?php include 'views/footer.php'; ?>
